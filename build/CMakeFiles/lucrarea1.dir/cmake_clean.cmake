647dfe8fbcd1b4e9ff26a09c7d0615d695ce6866
IzabellaKovacs <izabella.kovacs23@gmail.com>
2021-11-02 11:35:28 +0200

    Lucrarea 1

---
+++ build/CMakeFiles/lucrarea1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lucrarea1.dir/src/main.cpp.o"
+  "CMakeFiles/lucrarea1.dir/src/main.cpp.o.d"
+  "lucrarea1"
+  "lucrarea1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lucrarea1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
